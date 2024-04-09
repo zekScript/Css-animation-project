e7a208e16430c845a4b8f13273a9a9f4f8b922df
zekScript <164652091+zekScript@users.noreply.github.com>
2024-04-09 11:07:10 +0300

    Add files via upload

---
+++ CSS Animation/script.js
@@ -0,0 +1,15 @@
+// button program
+// make a button function when pressed display his name according to the value entered
+
+
+const outputFromInput = document.getElementById("outputFromInput");
+const outputName = document.getElementById("outputName");
+const myInput = document.getElementById("myInput");
+const myBtn = document.getElementById("myBtn");
+
+
+
+
+function onclickAnimation(){
+    
+}
